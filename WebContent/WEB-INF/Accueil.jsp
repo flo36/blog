7c4d0483bd0b55df11aebcb65e37bd0a1e3f6561
flo36 <63344794+flo36@users.noreply.github.com>
2020-04-26 11:10:58 +0200

    initialisation

---
+++ WebContent/WEB-INF/Accueil.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Inscription_Connexion</title>
+<link rel="stylesheet" href="././fichier_css/inscription_connexion.css" />
+</head>
+<body>
+	<h1> Bienvenue sur le blog</h1>
+	    	
+	    		<img src="././images/logo_orange.jpg" alt="logo" width="30%" >	
+	    	
+	    	<section>
+	    		  <button onclick="window.location.href = 'https://fr.w3docs.com/';" ><p>Inscription</p></button>
+	    		  <button onclick="window.location.href = 'https://fr.w3docs.com/';"><p>Connexion</p></button>
+	    	</section>
+</body>
+</html>
---
+++ src/Accueil.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Accueil
+ */
+@WebServlet("/Accueil")
+public class Accueil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Accueil() {
+    	
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		this.getServletContext().getRequestDispatcher("/WEB-INF/Accueil.jsp").forward(request, response);
+		
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
